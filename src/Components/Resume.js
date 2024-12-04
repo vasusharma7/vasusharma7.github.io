dedb045f389a51653f2878b4e88540ec1a3076ee
Vasu Sharma <40715071+vasusharma7@users.noreply.github.com>
2024-12-04 22:56:37 +0000

    re-ordered work/education

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -41,6 +41,16 @@ class Resume extends Component {
 
     return (
       <section id="resume">
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">{work}</div>
+        </div>
+
         <div className="row education">
           <div className="three columns header-col">
             <h1>
@@ -53,16 +63,6 @@ class Resume extends Component {
               <div className="twelve columns">{education}</div>
             </div>
           </div>
-        </div>
-
-        <div className="row work">
-          <div className="three columns header-col">
-            <h1>
-              <span>Work</span>
-            </h1>
-          </div>
-
-          <div className="nine columns main-col">{work}</div>
         </div>
 
         {/* <div className="row skill">
